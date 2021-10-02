cf56b9d362730f3c10ec342b814b34bf9eea0e3d
aemullin <amullin68@gmail.com>
2021-10-02 16:32:16 -0700

    Renamed app and created my pokemon list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favFood = "Spaghetti and Meatballs";
-document.write(favFood);
+let pokemonList=[
+  {name:'Bulbasaur', height: 1, type:'grass'},
+  {name:'Charmander', height: 2, type:'fire'},
+  {name:'Squirtle', height: 1, type:'water'}
+];
