42434d84b73e9cafd019333f96cd192c000b070b
aemullin <amullin68@gmail.com>
2021-10-06 21:48:07 -0700

    Create a loop that displays my pokemon list and labels tall pokemon
    Created a loop that displays pokemon along with their height and created a conditional that displayed a comment on taller pokemon.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ let pokemonList=[
   {name:'Charmander', height: 2, type:'fire'},
   {name:'Squirtle', height: 1, type:'water'}
 ];
+//list of pokemon
+
+for ( let i=0; i<pokemonList.length; i++){
+  document.write("<p>")
+  document.write(pokemonList[i].name + " " + "(" + "height:" + " " + pokemonList[i].height +")")
+  //loop displaying pokemon name and height
+  if (pokemonList[i].height>1){
+    document.write(" - So big!")
+  }
+  //add comment to pokemon larger than 2
+  document.write("</p>")
+}
