edddfe1cb1ea19fd172d91e0e36fd763b1c345b3
aemullin <amullin68@gmail.com>
2021-10-17 13:52:37 -0700

    Switch from for loop to foreach function

--- js/script.js
+++ js/script.js
@@ -3,15 +3,21 @@ let pokemonList=[
   {name:'Charmander', height: 2, type:'fire'},
   {name:'Squirtle', height: 1, type:'water'}
 ];
+
+pokemonList.forEach(function(pokemon) {
+  document.write("<p>" + pokemon.name + " (" + pokemon.height + " ft) - " + pokemon.type + "</p>" );
+});
+
+
 //list of pokemon
 
-for ( let i=0; i<pokemonList.length; i++){
-  document.write("<p>")
-  document.write(pokemonList[i].name + " " + "(" + "height:" + " " + pokemonList[i].height +")")
+//for ( let i=0; i<pokemonList.length; i++){
+  //document.write("<p>")
+  //document.write(pokemonList[i].name + " " + "(" + "height:" + " " + pokemonList[i].height +")")
   //loop displaying pokemon name and height
-  if (pokemonList[i].height>1){
-    document.write(" - So big!")
-  }
+  //if (pokemonList[i].height>1){
+    //document.write(" - So big!")
+  //}
   //add comment to pokemon larger than 2
-  document.write("</p>")
-}
+  //document.write("</p>")
+//}
