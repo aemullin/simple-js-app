934189656fe47a10b7194b4ac4d86b71f32fc63b
aemullin <amullin68@gmail.com>
2021-10-30 16:02:02 -0700

    Incorporate Bootstrap into app

--- js/script.js
+++ js/script.js
@@ -16,9 +16,7 @@ let pokemonRepository = (function () {
       console.log( "error")
     }
   }
-  function showDetails(pokemon){
-    console.log(pokemon.name)
-  }
+
   function loadList() {
     return fetch(apiURL).then(function (response) {
       return response.json();
@@ -51,22 +49,95 @@ let pokemonRepository = (function () {
   function addListItem(pokemon){
     let pokemonList=document.querySelector('.pokemon-list');
     let listItem=document.createElement('li');
+    listItem.classList.add('list-group-item');
     let button=document.createElement('button');
+    button.classList.add('pokemon-item')
     button.innerText=pokemon.name;
-    button.classList.add('pokemon');
     button.addEventListener('click', function(){
       showDetails(pokemon)
     });
+    button.setAttribute('type', 'button');
+    button.classList.add('pokemon', 'btn');
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#targetModal');
+
+
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
   }
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.setAttribute('id', 'targetModal');
+  modal.setAttribute('tabindex', '-1');
+  modal.setAttribute('role', 'dialog');
+  let modalDialog = document.createElement('div');
+  modalDialog.classList.add('modal-dialog');
+  modalDialog.setAttribute('role', 'document');
+  let modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  let modalHeader = document.createElement('div');
+  modalHeader.classList.add('modal-header');
+  let modalTitle = document.createElement('h1');
+  modalTitle.classList.add('modal-title');
+  let closeButton = document.createElement('button');
+  closeButton.setAttribute('type', 'button');
+  closeButton.classList.add('close');
+  closeButton.setAttribute('data-dismiss', 'modal');
+  closeButton.setAttribute('aria-label', 'Close');
+  closeButton.innerText = 'X';
+  let modalBody = document.createElement('div');
+  modalBody.classList.add('modal-body');
+  let pokemonImage = document.createElement('img');
+  pokemonImage.classList.add('pokemonimage');
+  let pokemonStat = document.createElement('div');
+  pokemonStat.classList.add('pokemon-stat');
+  let pokemonHeight = document.createElement('p');
+  let pokemonWeight = document.createElement('p');
+  let pokemonType = document.createElement('p');
+  document.body.appendChild(modal);
+  modal.appendChild(modalDialog);
+  modalDialog.appendChild(modalContent);
+  modalContent.appendChild(modalHeader);
+  modalHeader.appendChild(modalTitle);
+  modalHeader.appendChild(closeButton);
+  modalContent.appendChild(modalBody);
+  modalBody.appendChild(pokemonImage);
+  modalBody.appendChild(pokemonHeight);
+  modalBody.appendChild(pokemonWeight);
+  modalBody.appendChild(pokemonType);
+
+function showInfo(pokemon) {
+  modalTitle.innerText = pokemon.name;
+  pokemonImage.src = pokemon.imageUrl;
+  //decimeters most practically describes the height provided by the API so i convert the number to ft.
+  let height = (pokemon.height*0.328084)
+  height = height.toFixed(2)
+  pokemonHeight.innerText = 'Height: ' + height + ' ft';
+  pokemonHeight.classList.add('pokemon-height');
+  //Hectogram most practically describes the weight provided by the API so i convert the number to ft.
+  let weight = (pokemon.weight*0.220462)
+  weight = weight.toFixed(2)
+  pokemonWeight.innerText = 'Weight: ' + weight + ' lbs';
+  pokemonWeight.classList.add('pokemon-weight');
+    pokemonType.classList.add('pokemon-type');
+  if (pokemon.types[1]){
+    pokemonType.innerText = 'Type: ' + pokemon.types[0].type.name + ", " + pokemon.types[1].type.name
+  } else{
+    pokemonType.innerText = 'Type: ' + pokemon.types[0].type.name
+  }
+};
+
 function showDetails(pokemon) {
   loadDetails(pokemon).then(function () {
     console.log(pokemon);
-    showModal(pokemon);
+    showInfo(pokemon);
   });
 }
-let modalContainer = document.querySelector('#modal-container');
+
+
+
+/*let modalContainer = document.querySelector('#modal-container');
 function showModal(pokemon){
   modalContainer.innerHTML = ""
   let modal = document.createElement('div');
@@ -133,7 +204,7 @@ modalContainer.addEventListener('click', (e) => {
   if (target === modalContainer) {
     hideModal();
   }
-});
+}); */
 return{
     add: add,
     loadList: loadList,
